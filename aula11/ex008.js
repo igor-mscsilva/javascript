265b52c32adbd86e37041bebd86e04f88bfc99da
igor Silva <igor_mscsilva@outlook.com>
2023-02-01 08:31:43 -0300

    if e else em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var velo = 60.5
+console.log(`A velocidade do seu carro é ${velo}Km/h`)
+if ( velo > 65){ // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
