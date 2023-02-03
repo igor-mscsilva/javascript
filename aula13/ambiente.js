52fd989c85a4378b0951e7c4f799385abd7439c2
igor Silva <igor_mscsilva@outlook.com>
2023-02-03 11:58:43 -0300

    Adiciona estrutura for
    Foi feitos exemplos utilizando while, do while e for

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+//var c = 1
+
+/* ===Teste lógico no começo===
+
+while (c < 6){
+    console.log('Olá !')
+    c++ // c = c + 1
+}*/
+
+
+// ===Teste lógico no final===
+
+/*do{
+    console.log(`Numero: ${c}`)
+    c ++
+} while (c <= 6)
+*/
+
+// === Estrutura FOR ===
+
+//  Variavel  / Teste / Incremento 
+console.log('Vai começar !')
+for(var c = 1; c <= 6; c++){
+    console.log(`Numero: ${c}`)
+}
+
+console.log('FIM!')
