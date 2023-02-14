c428bd36a51ad091c81e643f64ddee39c1ca3436
igor Silva <igor_mscsilva@outlook.com>
2023-02-14 12:19:02 -0300

    rest operator

--- aula18/ex020 - Rest operator/script.js
+++ aula18/ex020 - Rest operator/script.js
@@ -1,10 +1,21 @@
-function cadastroPessoa(info){
-    let newdados = {
-        ...info,
-        cidade : 'SP',
-        status : 1
-    };
-    return newdados;
+//REST OPERATOR
+
+/*function minhalista(...nomes){ // adicionando o (...) ele ira mostrar todos nomes passados no parametro(minhalista)
+    console.log(nomes);
 }
 
-console.log(cadastroPessoa({nome: 'Igor', sobrenome: 'Silva', idade : 30}));
+minhalista('Igor', 'Mateus', 'João');*/
+
+function cadastrar(usuarios, ...newusers){
+    let totalusers = [
+        // juntar os 2
+        ...usuarios,
+        ...newusers
+    ];
+    return console.log(totalusers)
+
+}
+
+let usuarios = ['Igor', 'Marcos'];
+
+let newusers = cadastrar(usuarios, 'João', 'Murilo');
